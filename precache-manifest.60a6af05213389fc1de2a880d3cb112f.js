c18e64a33a77e8a3b857db07feb7be4cdd4c7684
dannyGan <dannyakobus@yahoo.com>
2019-11-18 11:16:42 +0700

    Updates

---
+++ precache-manifest.60a6af05213389fc1de2a880d3cb112f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fffac171ca6911e00e6dd303e417a275",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "b48ae1bee57fb8132678",
+    "url": "/tictactoe/static/css/main.7f57425d.chunk.css"
+  },
+  {
+    "revision": "79561395cfe401e879d7",
+    "url": "/tictactoe/static/js/2.55e0aed6.chunk.js"
+  },
+  {
+    "revision": "b48ae1bee57fb8132678",
+    "url": "/tictactoe/static/js/main.649dc5db.chunk.js"
+  },
+  {
+    "revision": "1db7538933510e121fef",
+    "url": "/tictactoe/static/js/runtime-main.0b8d60a0.js"
+  }
+]);
